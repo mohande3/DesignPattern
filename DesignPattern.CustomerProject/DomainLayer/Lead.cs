79d2c1cb663daabd5cd8c67169150d2500251adf
mohamd taghi khademi <mohande3khademi@hotmail.com>
2023-01-29 18:14:59 +0330

    Add Customer and lead entity

---
+++ DesignPattern.CustomerProject/DomainLayer/Lead.cs
@@ -0,0 +1,15 @@
+﻿//----------------------------------
+//-- Creator : MrMohande3 Khademi --
+//----------------------------------
+
+namespace DesignPattern.CustomerProject.DomainLayer
+{
+    public class Lead
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal BillAmount { get; set; }
+        public DateTime BillDate { get; set; }
+        public string Addess { get; set; }
+    }
+}
